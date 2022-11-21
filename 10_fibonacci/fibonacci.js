73af1a850751f6243561b9aa4ae1256f12ae5e86
olof-sky <oloof92@gmail.com>
2022-11-21 10:05:10 +0100

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  result = [1];
+  if (num < 0) return "OOPS";
+  for (let i = 0; i < num - 1; i++) {
+    if (result.length == 1) result.push(1);
+    else result.push(parseInt(result.slice(-1)) + parseInt(result.slice(-2)));
+  }
+  return parseInt(result.slice(-1));
 };
 
 // Do not edit below this line
